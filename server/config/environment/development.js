71628f77e0b8df601ab29ebcce4a2dfcaa0cba54
Charles C. Williams, Jr <chuckwilliams37@gmail.com>
2015-01-02 20:15:49 +0000

    Initial Commit post fullstack gen

---
+++ server/config/environment/development.js
@@ -0,0 +1,12 @@
+'use strict';
+
+// Development specific configuration
+// ==================================
+module.exports = {
+  // MongoDB connection options
+  mongo: {
+    uri: 'mongodb://cloud9:cloud9password@ds063240.mongolab.com:63240/ludum_vitae_0_0_1'
+  },
+
+  seedDB: true
+};
